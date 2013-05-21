6499e97ec88867d6d35bdcc24159c30cb8af1153
Elliott Hauser <eah13@mac.com>
2013-05-21 15:51:13 -0400

    Added fill-in-the-blank yourscript.sh

---
+++ yourscript.sh
@@ -0,0 +1,38 @@
+## Your first Script
+
+
+##
+## This script will do 3 things when it's done:
+## 1 - display all of your history items containing a certain command
+## 2 - display all lines of a text file containing a certain command
+## 3 - any UNIX command of your choice
+##
+
+## The comments will give you clues and tell you how to order things but you'll do all the work.
+
+
+## CHOOSE
+##   This section asks the user for their choice of 1, 2 or 3
+
+echo "Choose what function you want"
+echo " 1 - search your history"
+echo " 2 - search a file"
+echo " 3 - "                    # TODO: Fill this in with your command
+echo
+echo "Select 1, 2 or 3: "
+read CHOICE 
+
+## OK, from here on you'll have to finish the script.  Follow the comments and fill in more code:
+
+##
+## If CHOICE is 1, read in a COMMAND and display history items
+##
+
+
+##
+## If CHOICE is 2, read in a COMMAND and  a FILE and search FILE for COMMAND
+##
+
+##
+## If CHOICE is 3, execute any UNIX command of your choice
+##
